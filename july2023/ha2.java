797d8115c24d57b06e1c6c4808ff5f4baafb60d2
Sulim <sulim0314@naver.com>
2023-07-05 00:23:44 +0900

    난이도 하 5문제 커밋 30분만 빨리할걸..

---
+++ july2023/ha2.java
@@ -0,0 +1,22 @@
+package july2023;
+
+import java.util.Scanner;
+
+public class ha2 {
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int result = 0;
+		
+		while (n > 0) {
+            result += n % 10; 
+            n /= 10; 
+        }
+		
+		System.out.println(result);
+	}
+	
+}
