a87d5083f9cdc6458555a39c80c150174127451e
고수림 <sulim0314@naver.com>
2023-07-11 10:21:26 +0900

    알고리즘 공부

---
+++ src/com/sulim/homework/Main_10844_S1_쉬운계단수.java
@@ -0,0 +1,40 @@
+package com.sulim.homework;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_10844_S1_쉬운계단수 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		final long mod = 1000000000;
+		int N = Integer.parseInt(st.nextToken());
+		long[][] dp = new long[N+1][10];
+
+		for(int i=1; i<10; i++) {
+			dp[1][i] = 1;
+		}
+		
+		for(int i=2; i<=N; i++) {
+			for(int j=0; j<10; j++) {
+				if(j == 9) {
+					dp[i][9] = dp[i-1][8];
+				} else if(j == 0) {
+					dp[i][0] = dp[i-1][1];
+				} else {
+					dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod;
+				}
+			}
+		}
+		
+		long answer = 0;
+		for(int i=0; i<10; i++) {
+			answer += dp[N][i] % mod;
+		}
+		System.out.println(answer % mod);
+	}
+
+}
