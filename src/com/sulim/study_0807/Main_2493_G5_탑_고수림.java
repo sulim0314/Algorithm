31c3a86091e684b6b8454aeb749f182744ef1d97
수림 <125880884+sulim0314@users.noreply.github.com>
2023-08-07 14:25:41 +0900

    Create Main_2493_G5_탑_고수림.java
    백준 2493 문제

---
+++ src/com/sulim/study_0807/Main_2493_G5_탑_고수림.java
@@ -0,0 +1,50 @@
+package com.sulim.study_0807;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+class Top {
+	int height;
+	int num;
+}
+
+public class Main_2493_G5_탑_고수림 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		Stack<Top> s = new Stack<>();
+		
+		int N = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			Top top = new Top();
+			top.height = Integer.parseInt(st.nextToken());
+			top.num = i+1;
+			
+			while(!s.isEmpty() && s.peek().height < top.height) {
+				s.pop();
+			}
+			
+			if(s.isEmpty()) {
+				sb.append(0 + " ");
+			} else {
+				sb.append(s.peek().num + " ");
+			}
+			
+			s.push(top);
+
+		}
+		System.out.print(sb);
+		
+		br.close();
+	}
+
+}
