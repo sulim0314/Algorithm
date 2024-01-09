981960a4253bd91729f8055df5267234d6ef8e35
Sulim Koh <125880884+sulim0314@users.noreply.github.com>
2024-01-09 09:04:59 +0900

    [Silver III] Title: N과 M (4), Time: 576 ms, Memory: 21712 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	static int[] data, num; // nCm : 콤비.. 
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken()); 
+		M = Integer.parseInt(st.nextToken()); 
+		
+		data = new int[N];
+		num = new int[M];
+		
+		for(int i=0; i<N; i++) {
+			data[i] = i+1;
+		}
+		
+		combi(0,0);
+		
+	}
+
+	private static void combi(int cnt, int start) {
+		
+		if(cnt == M) {
+			for(int i=0; i<M; i++) {
+				System.out.print(num[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=start; i<N; i++) {
+			num[cnt] = data[i];
+			combi(cnt+1, i);
+		}
+		
+		
+	}
+		
+
+}
