505781d7f8b5e214070f994205954ecf9b683cb0
Sulim Koh <125880884+sulim0314@users.noreply.github.com>
2024-01-10 23:56:05 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1904 ms, Memory: 76404 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static ArrayList<Integer>[] list;
+	static int[] arr;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		
+		list = new ArrayList[N+1];
+		for(int i=0; i<N+1; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i=0; i<N-1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
+		}
+		
+		arr = new int[N+1];
+		visited = new boolean[N+1];
+		
+		Queue<Integer> q = new LinkedList<>();
+		q.add(1);
+		visited[1] = true;
+		
+		while(!q.isEmpty()) {
+			int n = q.poll();
+			
+			for(int i=0; i<list[n].size(); i++) {
+				int a = list[n].get(i);
+				if(!visited[a]) {
+					q.add(a);
+					visited[a] = true;
+					arr[a] = n;
+				}
+			}
+		}
+		
+		for(int i=2; i<=N; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		
+		
+		
+		
+	}
+
+}
