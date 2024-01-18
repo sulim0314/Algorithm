1389f19fed7addd2cee98d0455af9e07c1d3a79c
Sulim Koh <125880884+sulim0314@users.noreply.github.com>
2024-01-18 16:22:05 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int result = 0;
+		
+		for(int i=N-1; i>=0; i--) {
+			int temp = K / arr[i];
+			K = K % arr[i];
+			result += temp;
+		}
+		
+		System.out.println(result);
+	}
+
+}
