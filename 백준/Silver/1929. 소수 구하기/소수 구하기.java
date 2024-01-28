9a428540f47d3f02886002e91e1fc6e2d1b9d6d0
Sulim Koh <125880884+sulim0314@users.noreply.github.com>
2024-01-28 23:22:56 +0900

    [Silver III] Title: 소수 구하기, Time: 568 ms, Memory: 20836 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int M;
+	static int N;
+	static boolean flag;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		
+
+		for(int i=M; i<=N; i++) {
+			flag = true;
+			check(i);
+			if(flag && i!=1) {
+				bw.write(i+"\n");
+			}
+		}
+		
+		bw.flush();
+		bw.close();
+		
+	}
+
+	private static void check(int num) {
+
+		for(int i=2; i<=Math.sqrt(num); i++) {
+			if(num % i == 0) { // 소수가 아닌거
+				flag = false;
+				break;
+			} 
+		}
+		
+	}
+
+}
