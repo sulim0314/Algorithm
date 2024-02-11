9b4cc69f0f68016f7c853b4b1b9ed317f5679f41
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-02-11 23:45:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 616 ms, Memory: 49072 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static void make() {
+		parents = new int[V];
+		for(int i=0; i<V; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	static int find(int a) {
+		if(parents[a] == a) return a;
+		return parents[a] = find(parents[a]);
+	}
+	
+	static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		
+		if(aRoot == bRoot) return false;
+		
+		parents[bRoot] = aRoot;
+		return true;
+	}
+	
+	static class Edge implements Comparable<Edge> {
+		int from, to, weight;
+		
+		public Edge(int from, int to, int weight) {
+			super();
+			this.from = from;
+			this.to = to;
+			this.weight = weight;
+		}
+		
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+	}
+	
+	static int V, E;
+	static Edge[] edgeList;
+	static int[] parents;
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		edgeList = new Edge[E];
+		
+		for(int i=0; i<E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken())-1;
+			int to = Integer.parseInt(st.nextToken())-1;
+			int weight = Integer.parseInt(st.nextToken());
+			edgeList[i] = new Edge(from, to, weight);
+		}
+		
+		Arrays.sort(edgeList);
+		
+		make();
+		
+		long result = 0;
+		int count = 0;
+		for(Edge edge: edgeList) {
+			if(union(edge.from, edge.to)) {
+				result += edge.weight;
+				if(++count == V-1) break;
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+
+}
