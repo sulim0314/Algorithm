cef1acc44bc0421f00d9d093d61d004b3d7dbcd1
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-07-18 14:24:12 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 560 ms, Memory: 48612 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,85 +1,80 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static void make() {
-		parents = new int[V];
-		for(int i=0; i<V; i++) {
-			parents[i] = i;
-		}
-	}
-	
-	static int find(int a) {
-		if(parents[a] == a) return a;
-		return parents[a] = find(parents[a]);
-	}
-	
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		
-		if(aRoot == bRoot) return false;
-		
-		parents[bRoot] = aRoot;
-		return true;
-	}
-	
-	static class Edge implements Comparable<Edge> {
-		int from, to, weight;
-		
-		public Edge(int from, int to, int weight) {
-			super();
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
-		}
-		
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.weight, o.weight);
-		}
-	}
-	
-	static int V, E;
-	static Edge[] edgeList;
-	static int[] parents;
-	
-	public static void main(String[] args) throws Exception {
+    static class Info implements Comparable<Info> {
+        int from, to, weight;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		
-		edgeList = new Edge[E];
-		
-		for(int i=0; i<E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken())-1;
-			int to = Integer.parseInt(st.nextToken())-1;
-			int weight = Integer.parseInt(st.nextToken());
-			edgeList[i] = new Edge(from, to, weight);
-		}
-		
-		Arrays.sort(edgeList);
-		
-		make();
-		
-		long result = 0;
-		int count = 0;
-		for(Edge edge: edgeList) {
-			if(union(edge.from, edge.to)) {
-				result += edge.weight;
-				if(++count == V-1) break;
-			}
-		}
-		
-		System.out.println(result);
-		
-	}
+        public Info(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
 
+        @Override
+        public int compareTo(Info o) {
+            return this.weight - o.weight;
+        }
+    }
+
+    static int V, E;
+    static int[] parents;
+    static Info[] infoList;
+
+    private static void init() {
+        parents = new int[V + 1];
+        for(int i = 0; i <= V; i++) {
+            parents[i] = i;
+        }
+    }
+
+    private static int find(int a) {
+        if(parents[a] == a) return a;
+        return parents[a] = find(parents[a]);
+    }
+
+    private static boolean union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if(pa == pb) return false; // 다 이어짐
+
+        parents[pa] = pb;
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        infoList = new Info[E];
+        init();
+
+        for(int i=0; i<E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            infoList[i] = new Info(from, to, weight);
+        }
+
+        Arrays.sort(infoList); // weight 작은거부터 큰 순서대로
+
+        long result = 0;
+        int cnt = 0;
+        for(int i=0; i<E; i++) {
+            Info cur = infoList[i];
+            if(union(cur.from, cur.to)) { // 합칠 수 있을 때
+                result += cur.weight;
+                if(++cnt == V-1) break;
+            }
+        }
+
+        System.out.println(result);
+
+    }
 }
