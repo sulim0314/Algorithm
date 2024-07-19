4e0f4b9f67ecff007e2c5975e003bbf08e67de62
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-07-19 13:30:10 +0900

    [Gold III] Title: 줄 세우기, Time: 1064 ms, Memory: 56008 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,81 +1,60 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int N,M;
-	
-	static int[] inDegree; // 각 노드의 진입차수
-	static ArrayList<Integer>[] adjLists;
-	static StringBuilder sb;
-	
-	public static void main(String[] args) throws Exception {
+    static int N, M;
+    static int[] num;
+    static ArrayList<Integer>[] list;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		sb = new StringBuilder();
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		adjLists = new ArrayList[N + 1];
-		
-		for(int i=0; i<=N; i++) {
-			adjLists[i] = new ArrayList<Integer>();
-		}
-		
-		// 각 노드의 진입차수
-		inDegree = new int[N + 1];
-		
-		// 방향 그래프
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int head = Integer.parseInt(st.nextToken());
-			int adj = Integer.parseInt(st.nextToken());
-			adjLists[head].add(adj); // 단방향 연결 설정
-			inDegree[adj]++; // 후행 정점 진입 차수 증가
-		}
-		
-		// 위상 정렬 (A B: A가 B앞에 선다. A가 선행)
-		topologicalSort();
-		
-	}
+    public static void main(String[] args) throws Exception {
 
-	private static void topologicalSort() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		Queue<Integer> q = new LinkedList<>();
-		
-		// 초기: 선행 정점을 가지지 않은 정점을 큐에 저장.
-		for(int i=1; i<=N; i++) {
-			if(inDegree[i] == 0) { // 진입차수가 0인 노드 저장
-				q.add(i);
-			}
-		}
-		
-		// 정점의 개수 만큼 반복
-		for(int i=0; i<N; i++) {
-			int N = q.poll(); // 1.큐에서 정점 후보 추출 및 삭제
-			sb.append(N+" ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[N + 1];
+        list = new ArrayList[N + 1];
+        for(int i=0; i<N+1; i++) {
+            list[i] = new ArrayList<>();
+        }
 
-			// 2. 해당 정점과 연결된 모든 정점에 대해
-			//for (int nextV : adjLists[N]) {
-			for (int j=0,size=adjLists[N].size(); j<size; j++) {
-				int nextV = adjLists[N].get(j);
-				inDegree[nextV]--;// 2.1 진입 차수 감소
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()); // 작은 애
+            int b = Integer.parseInt(st.nextToken()); // 큰 애
+            list[s].add(b);
+            num[b]++;
+        }
 
-				// 2-2. 선행 정점을 가지지 않는 정점을 큐에 저장
-				if (inDegree[nextV] == 0) {
-					q.add(nextV);
-				}
-			}
-		}
-		
-		System.out.println(sb);
-		
-	}
+        topologySort();
+
+    }
+
+    private static void topologySort() {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=1; i<N+1; i++) {
+            if(num[i] == 0) {
+                pq.add(i);
+            }
+        }
+
+        while(!pq.isEmpty()) {
+            int cur = pq.poll();
+            System.out.print(cur + " ");
+
+            for(int i=0; i<list[cur].size(); i++) {
+                int next = list[cur].get(i);
+                num[next]--;
+                if(num[next] == 0) {
+                    pq.add(next);
+                }
+            }
+        }
+
+    }
 
 }
