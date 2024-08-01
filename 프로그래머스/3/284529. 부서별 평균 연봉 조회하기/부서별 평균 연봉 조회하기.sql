c4664b27af23dceeab300e34ae551c8b9050145e
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-08-01 20:54:03 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -0,0 +1,11 @@
+select 
+    a.dept_id, 
+    a.dept_name_en, 
+    round(avg(b.sal),0) as avg_sal
+from HR_DEPARTMENT a
+
+join HR_EMPLOYEES b
+on a.dept_id = b.dept_id
+
+group by a.dept_id
+order by 3 desc;
