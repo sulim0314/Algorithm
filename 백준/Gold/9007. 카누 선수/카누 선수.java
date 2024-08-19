792e6d678726907d1397bb9a3e031bd38d96c845
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-08-19 17:47:35 +0900

    [Gold II] Title: 카누 선수, Time: 3772 ms, Memory: 120784 KB -BaekjoonHub

--- 백준/Gold/9007. 카누 선수/카누 선수.java
+++ 백준/Gold/9007. 카누 선수/카누 선수.java
@@ -1,94 +1,94 @@
 import java.util.*;
 import java.io.*;
 
-// 카누선수, 이분탐색
 public class Main {
 
     static int k, n;
-    static long[] tmp;
-    static long[] arr12, arr34;
-    static long abs, result;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int T = Integer.parseInt(st.nextToken());
-        long weight;
-        for (int tc = 1; tc <= T; tc++) {
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
             st = new StringTokenizer(br.readLine());
-            k = Integer.parseInt(st.nextToken()); // 합해서 돼야 할 숫자
+            k = Integer.parseInt(st.nextToken());
             n = Integer.parseInt(st.nextToken());
 
-            tmp = new long[n];
-            // 1반 학생들의 몸무게
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                tmp[i] = Long.parseLong(st.nextToken());
-            }
-            // 2반 학생들의 몸무게 추가 입력 (1반, 2반 합쳐서 나올 수 있는 경우의 수 모두)
-            arr12 = new long[n * n];
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                weight = Long.parseLong(st.nextToken());
+            int[][] group = new int[4][n];
+
+            for (int i = 0; i < 4 ; i++) {
+                st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < n; j++) {
-                    arr12[n * i + j] = weight + tmp[j];
-                }
-            }
-            // 3반 학생들의 몸무게
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                tmp[i] = Long.parseLong(st.nextToken());
-            }
-            // 4반 학생들의 몸무게 추가 입력
-            arr34 = new long[n * n];
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                weight = Long.parseLong(st.nextToken());
-                for (int j = 0; j < n; j++) {
-                    arr34[n * i + j] = weight + tmp[j];
+                    group[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            Arrays.sort(arr34);
-
-            abs = Math.max(k - 3, 40_000_001 - k);
-            for(int i=0; i<n*n; i++) {
-                solve(arr12[i]);
-                if(result == k) break;
+            int[][] calcGroup = new int[2][n*n];
+            int index = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    calcGroup[0][index] = group[0][i] + group[1][j];
+                    calcGroup[1][index++] = group[2][i] + group[3][j];
+                }
             }
 
-            sb.append(result).append("\n");
+            Arrays.sort(calcGroup[0]);
+            Arrays.sort(calcGroup[1]);
+
+            sb.append(k - find(calcGroup[0], calcGroup[1])).append('\n');
         }
 
         System.out.println(sb);
+        br.close();
     }
 
-    private static void solve(long num) {
-        int start = 0;
-        int end = n*n -1;
-        int mid;
-        long tmp;
-        while(start <= end) {
-            mid = (start + end)/2;
-            tmp = arr34[mid] + num - k;
+    public static int find(int[] first, int[] second) {
 
-            if(abs > Math.abs(tmp)) {
-                abs = Math.abs(tmp);
-                result = arr34[mid] + num;
-            } else if(abs == Math.abs(tmp) && tmp < 0) {
-                result = arr34[mid] + num;
+        int ans = 0, min = Integer.MAX_VALUE;
+        for (int i : first) {
+            int result = binarySearch(second, k - i);
+            int abs = Math.abs(result);
+
+            if (min > abs) {
+                ans = result;
+                min = abs;
+            } else if (min == abs && ans < 0) {
+                ans = result;
             }
-
-            if(arr34[mid] + num < k) {
-                start = mid + 1;
-            } else if(arr34[mid] + num == k) {
-                return;
-            } else {
-                end = mid - 1;
-            }
-
         }
+
+        return ans;
+    }
+    public static int binarySearch(int[] arr, int num) {
+
+        int result = 0, min = Integer.MAX_VALUE;
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+
+            int calc = num - arr[mid];
+            int abs = Math.abs(calc);
+
+            if (min > abs) {
+                min = abs;
+                result = calc;
+            } else if (min == abs && result < 0) {
+                result = calc;
+            }
+
+            if (calc > 0) {
+                left = mid + 1;
+            } else if (calc < 0) {
+                right = mid - 1;
+            } else {
+                return 0;
+            }
+        }
+
+        return result;
     }
 }
