1fe9d3c41e5892a0046b8a8e3a56eb1507a4e425
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-16 18:01:17 +0900

    [level 3] Title: 야근 지수, Time: 126.03 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,32 +1,6 @@
-import java.util.*;
-
 class Solution {
-    
     public long solution(int n, int[] works) {
         long answer = 0;
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0; i<works.length; i++) {
-            pq.add(works[i]);
-        }
-        
-        for(int i=0; i<n; i++) {
-            int cur = pq.poll();
-            
-            if(cur == 0) {
-                answer = 0;
-                break;
-            }
-            
-            cur--;
-            pq.add(cur);
-        }
-        
-        while(!pq.isEmpty()) {
-            int cur = pq.poll();
-            answer += Math.pow(cur, 2);
-        }
-        
         return answer;
     }
 }
