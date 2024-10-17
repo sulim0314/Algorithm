5c809b3266ab50dd468efffa33131036ce126ce8
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-17 16:15:36 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,10 +1,24 @@
 -- 코드를 입력하세요
-SELECT A.WRITER_ID AS USER_ID, B.NICKNAME, SUM(A.PRICE) AS TOTAL_SALES
-FROM USED_GOODS_BOARD A, USED_GOODS_USER B
-WHERE A.WRITER_ID = B.USER_ID AND A.STATUS = 'DONE'
-GROUP BY 
-    A.WRITER_ID, 
-    B.NICKNAME
-HAVING
-    SUM(A.PRICE) >= 700000
-ORDER BY TOTAL_SALES ASC;
+-- SELECT A.WRITER_ID, B.NICKNAME, SUM(A.PRICE) AS TOTAL_SALES
+-- FROM USED_GOODS_BOARD A, USED_GOODS_USER B
+-- WHERE A.WRITER_ID = B.USER_ID AND A.STATUS = 'DONE'
+-- GROUP BY 
+--     A.WRITER_ID, 
+--     B.NICKNAME
+-- HAVING
+--     SUM(A.PRICE) >= 700000
+-- ORDER BY TOTAL_SALES ASC;
+
+-- 완료된 중고거래, 총 금액 70만원 이상인 사람
+-- 회원 id, 닉네임, 총거래금액
+
+SELECT a.user_id, a.nickname, sum(b.price) as total_sales
+FROM USED_GOODS_user a, USED_GOODS_BOARD b
+
+where a.user_id = b.writer_id
+    and b.status = 'DONE'
+
+group by a.user_id, a.nickname
+having sum(b.price) >= 700000
+
+order by 3 asc;
