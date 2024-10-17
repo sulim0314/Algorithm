c10d46cf084164fa53aa36ed2e7d87700c4f704c
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-17 15:49:33 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,11 +1,21 @@
 -- 코드를 입력하세요
--- animalID ins엔 있는데 out엔 없는 것
-SELECT a.NAME, a.DATETIME
+# SELECT a.NAME, a.DATETIME, ROW_NUMBER() OVER (ORDER BY a.DATETIME ASC) as num
+# from animal_ins a
+# where a.animal_id not in (
+#         select animal_id
+#         from animal_outs
+#     )
+# order by 2 asc
+# limit 3;
+
+-- 아직 입양을 못 간 동물 중, 가장 오래 보호소에 있었던 동물 3마리의 이름, 보호 시작일
+select 
+    a.name, a.datetime
 from animal_ins a
-where a.animal_id not in (
+where a.animal_id 
+    not in (
         select animal_id
         from animal_outs
     )
 order by 2 asc
 limit 3;
-
