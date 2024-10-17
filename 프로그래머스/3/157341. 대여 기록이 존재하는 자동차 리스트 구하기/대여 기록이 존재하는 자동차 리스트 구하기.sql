edc85271ad44fd8960eb685fbc1e8080882bc435
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-17 16:07:40 +0900

    [level 3] Title: 대여 기록이 존재하는 자동차 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
+++ 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -1,7 +1,17 @@
 -- 코드를 입력하세요
-SELECT DISTINCT(A.CAR_ID)
-FROM CAR_RENTAL_COMPANY_CAR A
-LEFT JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B
-    ON A.CAR_ID = B.CAR_ID
-WHERE MONTH(B.START_DATE) = '10' AND CAR_TYPE='세단'
-ORDER BY 1 DESC;
+# SELECT DISTINCT(A.CAR_ID)
+# FROM CAR_RENTAL_COMPANY_CAR A
+# LEFT JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B
+#     ON A.CAR_ID = B.CAR_ID
+# WHERE MONTH(B.START_DATE) = '10' AND CAR_TYPE='세단'
+# ORDER BY 1 DESC;
+
+-- 자동차 종류가 세단
+-- 10월에 대여를 시작한 기록이 있는,, 중복ㄴ,
+select distinct(a.car_id)
+from car_rental_company_car a
+
+right join CAR_RENTAL_COMPANY_RENTAL_HISTORY b
+    on a.car_id = b.car_id
+where month(b.start_date) = '10' and car_type='세단'
+order by 1 desc;
