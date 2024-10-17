d78463ea51c2f8b64176b6d5ddc9580aaa5d1db0
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-17 15:59:22 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,7 +1,14 @@
 -- 코드를 입력하세요
-SELECT A.ANIMAL_ID, A.NAME
-FROM ANIMAL_OUTS A
-JOIN ANIMAL_INS B
-    ON A.ANIMAL_ID = B.ANIMAL_ID
-ORDER BY TIMESTAMPDIFF(SECOND, B.DATETIME, A.DATETIME) DESC
-LIMIT 2;
+# SELECT A.ANIMAL_ID, A.NAME
+# FROM ANIMAL_OUTS A
+# JOIN ANIMAL_INS B
+#     ON A.ANIMAL_ID = B.ANIMAL_ID
+# ORDER BY TIMESTAMPDIFF(SECOND, B.DATETIME, A.DATETIME) DESC
+# LIMIT 2;
+
+select a.animal_id, a.name
+from animal_ins a
+join animal_outs b
+    on a.animal_id = b.animal_id
+order by (b.datetime - a.datetime) desc
+limit 2;
