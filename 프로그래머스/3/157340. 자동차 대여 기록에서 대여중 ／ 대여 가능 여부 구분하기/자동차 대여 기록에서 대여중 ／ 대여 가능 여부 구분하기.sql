55df3d6fc785a72d1a3c9ebb5234a1d02ba6c7b5
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-17 19:45:28 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,16 +1,43 @@
-WITH tmp AS (
-    SELECT car_id
+# WITH 
+#     tmp AS (
+#         SELECT car_id
+#         FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+#         WHERE '2022-10-16' BETWEEN start_date AND end_date
+#         GROUP BY car_id
+#     ),
+#     c AS (
+#         SELECT DISTINCT car_id 
+#         FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+#     )
+
+# SELECT
+#     c.car_id,
+#     CASE 
+#         WHEN t.car_id IS NOT NULL THEN '대여중'
+#         ELSE '대여 가능'
+#     END AS AVAILABILITY
+# FROM c
+# LEFT JOIN tmp t ON c.car_id = t.car_id
+# ORDER BY c.car_id DESC;
+
+-- 2022.10.16에 대여중이면 대여중
+
+WITH tmp1 AS (
+    SELECT car_id, start_date, end_date
     FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
     WHERE '2022-10-16' BETWEEN start_date AND end_date
-    GROUP BY car_id
 )
 
-SELECT
-    c.car_id,
+SELECT a.car_id, 
     CASE 
-        WHEN t.car_id IS NOT NULL THEN '대여중'
+        WHEN tmp1.car_id IS NOT NULL THEN '대여중'
         ELSE '대여 가능'
     END AS AVAILABILITY
-FROM (SELECT DISTINCT car_id FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY) c
-LEFT JOIN tmp t ON c.car_id = t.car_id
-ORDER BY c.car_id DESC;
+    
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY a
+
+LEFT JOIN tmp1 ON a.car_id = tmp1.car_id
+GROUP BY a.car_id
+ORDER BY a.car_id DESC;
+
+
