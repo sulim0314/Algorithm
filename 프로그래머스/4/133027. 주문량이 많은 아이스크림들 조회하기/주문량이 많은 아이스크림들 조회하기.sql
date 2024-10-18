d6d6e28bb7d924a77df4a809690c32c102d35c52
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-18 17:00:13 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,18 +1,33 @@
 -- 코드를 입력하세요
-SELECT A.FLAVOR
-FROM (
-    SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOTAL_ORDER
-    FROM FIRST_HALF
-    GROUP BY FLAVOR
-    ) A
+# SELECT A.FLAVOR
+# FROM (
+#     SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOTAL_ORDER
+#     FROM FIRST_HALF
+#     GROUP BY FLAVOR
+#     ) A
 
-JOIN (
-    SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOTAL_ORDER
-    FROM JULY
-    GROUP BY FLAVOR
-    ) B
+# JOIN (
+#     SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOTAL_ORDER
+#     FROM JULY
+#     GROUP BY FLAVOR
+#     ) B
     
-    ON A.FLAVOR = B.FLAVOR
+#     ON A.FLAVOR = B.FLAVOR
 
-ORDER BY (A.TOTAL_ORDER + B.TOTAL_ORDER) DESC
-LIMIT 3;
+# ORDER BY (A.TOTAL_ORDER + B.TOTAL_ORDER) DESC
+# LIMIT 3;
+
+-- 7월 아이스크림 총 주문량과 상반기의 아이스크림 총 주문량을 더한 값이 큰 순서대로 3개
+
+with tmp1 as (
+    select flavor, sum(total_order) as total
+    from july
+    group by flavor
+)
+
+select f.flavor 
+from FIRST_HALF f
+join tmp1 t on t.flavor = f.flavor
+order by (t.total + f.total_order) desc
+limit 3;
+
