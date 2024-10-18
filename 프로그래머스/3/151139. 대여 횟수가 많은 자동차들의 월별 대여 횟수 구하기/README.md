d01a8ecf5981334b8ea233368325ebf0f422bf34
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-18 16:51:13 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기 - 151139 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151139?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2023년 10월 6일 21:33:46
+2024년 10월 18일 16:51:12
 
 ### 문제 설명
 
--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,16 +1,22 @@
-SELECT  EXTRACT (MONTH FROM start_date) AS "MONTH",CAR_ID, count(*) AS "RECORDS"
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-where start_date 
-    BETWEEN TO_DATE('2022-08-01', 'YYYY-MM-DD')
-    AND TO_DATE('2022-10-31', 'YYYY-MM-DD')
-    AND CAR_ID IN ( SELECT DISTINCT CAR_ID
-                    FROM
-                        (SELECT CAR_ID, COUNT(*)
-                           FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                           WHERE start_date 
-                                 BETWEEN TO_DATE('2022-08-01', 'YYYY-MM-DD')
-                                 AND TO_DATE('2022-10-31', 'YYYY-MM-DD')
-                           GROUP BY CAR_ID
-                           HAVING COUNT(*) >= 5 ))
-GROUP BY  EXTRACT (MONTH FROM start_date),CAR_ID 
-ORDER BY EXTRACT(MONTH FROM start_date),CAR_ID DESC;
+-- 대여 시작일을 기준으로 2022.08.01~ 2022.10.31 까지 총 대여 횟수가 5회 이상
+-- 해당 기간 동안 월결 자동차 id 별 총 대여 횟수 
+-- 월 오름차순, 자동차 id 내림차순, 특정 월 총 대여횟수 0은 제외
+
+with tmp1 as (
+    select car_id
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    where start_date between '2022-08-01' and '2022-10-31'
+    group by car_id
+    having count(*) >= 5
+)
+
+select month(c.start_date) as month, c.car_id, count(*) as records
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY c
+
+where c.car_id in (
+        select car_id
+        from tmp1
+    ) AND start_date BETWEEN '2022-08-01' AND '2022-10-31'
+    
+group by month, c.car_id
+order by 1 asc, 2 desc;
