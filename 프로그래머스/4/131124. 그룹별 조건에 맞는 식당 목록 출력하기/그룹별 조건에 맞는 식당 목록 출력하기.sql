1181605e004e7894bd377996ec3fe7c4baae24e6
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-18 17:30:02 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,17 +1,37 @@
+# with tmp as (
+#     select count(*), member_id
+#     from rest_review
+#     group by member_id
+#     order by 1 desc
+#     limit 1
+# )
+
+# SELECT
+#     m.MEMBER_NAME,
+#     r.REVIEW_TEXT,
+#     date_format(r.REVIEW_DATE, '%Y-%m-%d')
+# from MEMBER_PROFILE m
+# join REST_REVIEW r
+#     on m.member_id = r.member_id
+# where m.member_id = (select member_id from tmp)
+# order by 3 asc, 2 asc;
+
+
+-- 리뷰를 가장 많이 작성한 회원들의 리뷰
+-- 이름, 리뷰, 작성일
 with tmp as (
-    select count(*), member_id
-    from rest_review
+    select *
+    from REST_REVIEW
     group by member_id
-    order by 1 desc
+    order by count(*) desc
     limit 1
 )
 
-SELECT
-    m.MEMBER_NAME,
-    r.REVIEW_TEXT,
-    date_format(r.REVIEW_DATE, '%Y-%m-%d')
+select m.member_name, r.review_text, date_format(r.review_date, '%Y-%m-%d') as review_date
 from MEMBER_PROFILE m
-join REST_REVIEW r
-    on m.member_id = r.member_id
-where m.member_id = (select member_id from tmp)
+join REST_REVIEW r on r.member_id = m.member_id
+where m.member_id in (
+            select member_id
+            from tmp
+        )
 order by 3 asc, 2 asc;
