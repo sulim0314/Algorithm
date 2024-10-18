fda0de2e3ec77e5c8980ee088b0c9d2a06ba699a
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-18 18:16:10 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,11 +1,26 @@
 -- 코드를 작성해주세요
 
-SELECT ID, FISH_NAME, LENGTH
-FROM FISH_INFO JOIN FISH_NAME_INFO USING(FISH_TYPE)
+# SELECT ID, FISH_NAME, LENGTH
+# FROM FISH_INFO JOIN FISH_NAME_INFO USING(FISH_TYPE)
 
-WHERE (FISH_TYPE, LENGTH)
-    IN (
-        SELECT FISH_TYPE, MAX(LENGTH) AS LENGTH
-        FROM FISH_INFO
-        GROUP BY FISH_TYPE
-    );
+# WHERE (FISH_TYPE, LENGTH)
+#     IN (
+#         SELECT FISH_TYPE, MAX(LENGTH) AS LENGTH
+#         FROM FISH_INFO
+#         GROUP BY FISH_TYPE
+#     );
+
+
+-- 물고기 종류 별로 가장 큰 물고기 id, 이름, 길이
+with tmp as (
+    select fish_type, MAX(length) as len
+    from FISH_INFO
+    group by fish_type
+)
+
+select f.id, fn.fish_name, f.length
+from FISH_INFO f
+join tmp t on f.fish_type = t.fish_type
+join fish_name_info fn on fn.fish_type = f.fish_type
+where f.length = t.len
+order by 1 asc;
