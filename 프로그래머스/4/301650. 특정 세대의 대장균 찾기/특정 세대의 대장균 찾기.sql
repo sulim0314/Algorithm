5697043dd63583c34911cd01deac109ab9391572
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-18 19:10:23 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,21 +1,41 @@
-with recursive generation as (
-    select 
-        id,
-        parent_id,
-        1 as gen
-    from ECOLI_DATA
-    WHERE PARENT_ID IS NULL
-    union all
-    select
-        e.id,
-        e.parent_id,
-        g.gen + 1
-    from ECOLI_DATA e
+# with recursive generation as (
+#     select 
+#         id,
+#         parent_id,
+#         1 as gen
+#     from ECOLI_DATA
+#     WHERE PARENT_ID IS NULL
     
-    inner join generation g on g.id = e.parent_id
+#     union all
+    
+#     select
+#         e.id,
+#         e.parent_id,
+#         g.gen + 1
+#     from ECOLI_DATA e
+    
+#     inner join generation g on g.id = e.parent_id
+# )
+
+# select * from generation;
+
+# select id
+# from generation
+# where gen = 3
+# order by id;
+
+with recursive gen as (
+    select id, parent_id, 1 as gg
+    from ECOLI_DATA
+    where parent_id is null
+    
+    union all
+    
+    select e.id, e.parent_id, g.gg+1
+    from ECOLI_DATA e
+    join gen g on g.id = e.parent_id
 )
 
 select id
-from generation
-where gen = 3
-order by id;
+from gen
+where gg = 3;
