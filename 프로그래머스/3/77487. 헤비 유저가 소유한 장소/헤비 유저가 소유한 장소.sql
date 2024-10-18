bec79f0a2a8a2edbf3895ac4e9ac2ceeda3fb7b1
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-10-18 15:45:11 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,19 +1,30 @@
 -- 코드를 입력하세요
+# with tmp as (
+#     select
+#         host_id as h,
+#         count(*) as c
+#     from places
+#     group by host_id
+# )
+
+# SELECT 
+#     p.id,
+#     p.name,
+#     p.host_id
+# from places p
+
+# join tmp t on t.h = p.host_id
+# where t.c >= 2
+
+# order by 1 asc;
 with tmp as (
-    select
-        host_id as h,
-        count(*) as c
+    select host_id, count(*) as num
     from places
     group by host_id
 )
 
-SELECT 
-    p.id,
-    p.name,
-    p.host_id
+select p.id, p.name, p.host_id
 from places p
-
-join tmp t on t.h = p.host_id
-where t.c >= 2
-
+join tmp t on p.host_id = t.host_id
+where t.num >= 2
 order by 1 asc;
