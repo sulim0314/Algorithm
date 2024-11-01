258d27f1cad26e0c42181e3d3c1e2cd361ec14b2
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-11-01 16:46:45 +0900

    [Gold III] Title: 양팔저울, Time: 72 ms, Memory: 13224 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N; // N <= 30
+    static int[] w;
+    static boolean[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        w = new int[N];
+        dp = new boolean[N+1][40001]; // i: 추 몇개, j: 무게
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            w[i] = num;
+        }
+
+        solve(0, 0);
+
+        int c = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<c; i++) {
+            int check = Integer.parseInt(st.nextToken());
+            if(dp[N][check]) {
+                sb.append("Y ");
+            } else {
+                sb.append("N ");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+    private static void solve(int cnt, int num) {
+
+        if(dp[cnt][num]) return;
+        dp[cnt][num] = true;
+
+        if(cnt == N) return; // 마지막
+
+        solve(cnt+1, num + w[cnt]);
+        solve(cnt+1, num);
+        solve(cnt+1, Math.abs(num - w[cnt]));
+
+    }
+}
