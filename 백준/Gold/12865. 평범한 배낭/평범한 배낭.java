df7f63769b443395fcb410a0029fbf9753f5f4dc
고수림 <125880884+sulim0314@users.noreply.github.com>
2024-11-01 15:14:25 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51352 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,42 +1,49 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int N, K;
+    static int N, K;
+    static int[][] dp;
+    static int[] W, V;
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 물건 수
+        K = Integer.parseInt(st.nextToken()); // 준서가 버틸 수 있는 무게
+        W = new int[N+1];
+        V = new int[N+1];
 
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+        for(int i=1; i<=N; i++) {
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
+        }
 
-		int[][] item = new int[N + 1][2]; // weight, value
+        // dp[x][y] : 최대 무게 y인 가방에서, x번째 물건까지의 최대 가치
+        dp = new int[N+1][K+1];
 
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
+        for(int i=1; i<=N; i++) { // i번째 물건까지 고려함.
+            for(int j=1; j<=K; j++) { // 현재 담을 수 있는 최대 무게 j.
 
-			item[i][0] = w;
-			item[i][1] = v;
-		}
+                if(W[i] > j) { // 더 담을 수 없음
+                    dp[i][j] = dp[i-1][j];
+                }
 
-		int[][] dp = new int[N + 1][K + 1];
+                else { // 더 담을 수 있음
+                                // 안담은거, 담은거 의 max
+                                                  // i-1 번째 물건까지 고려했을 때,
+                                                  // j-W[i]의 가방에 담을 수 있는 가치에 더하기
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-W[i]] + V[i]);
+                }
 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= K; j++) {
-				if (j - item[i][0] >= 0) {
-					dp[i][j] = Math.max(dp[i - 1][j], item[i][1] + dp[i - 1][j - item[i][0]]);
-				} else {
-					dp[i][j] = dp[i - 1][j];
-				}
-			}
-		}
-		System.out.println(dp[N][K]);
-	}
+            }
+        }
+
+        System.out.println(dp[N][K]);
+
+    }
 
 }
