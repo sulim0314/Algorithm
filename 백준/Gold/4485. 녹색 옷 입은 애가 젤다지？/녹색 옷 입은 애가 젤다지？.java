5236f5ee04800f2d5bb623d893ab769ee8556f42
고수림 <125880884+sulim0314@users.noreply.github.com>
2025-02-04 18:40:52 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 152 ms, Memory: 17280 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -5,94 +5,88 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Node implements Comparable<Node> {
-		int r, c, weight;
-		public Node(int r, int c, int weight) {
-			this.r = r;
-			this.c = c;
-			this.weight = weight;
-		}
-		@Override
-		public int compareTo(Node o) {
-			return Integer.compare(this.weight, o.weight);
-		}
-	}
-	
-	static final int INF = Integer.MAX_VALUE;
-	static int[][] map;
-	static int[][] dist;
-	static int N, result;
-	static int[] dr = {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
-	
-	public static void main(String[] args) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int tc = 0;
-		while(true) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			if(N == 0) return;
-			
-			tc++;
-			map = new int[N][N];
-			dist = new int[N][N];
-			
-			for(int i=0; i<N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j=0; j<N; j++) {
-					int w = Integer.parseInt(st.nextToken());
-					map[i][j] = w;
-				}
-			}
-			
-			result = dijk();
-			System.out.println("Problem "+tc+": "+result);
-		}
-		
-	}
+    static class Info implements Comparable<Info> {
+        int x, y, w;
 
-	private static int dijk() {
+        public Info(int x, int y, int w) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+        }
 
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		boolean[][] check = new boolean[N][N];
-		dist = new int[N][N];
-		pq.add(new Node(0, 0, map[0][0]));
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				dist[i][j] = INF;
-			}
-		}
-		dist[0][0] = map[0][0];
-		
-		
-		while(!pq.isEmpty()) {
-			Node curNode = pq.poll();
-			int curR = curNode.r;
-			int curC = curNode.c;
-			
-			if(check[curR][curC] == true) continue;
-			check[curR][curC] = true;
-			
-			// 4방향
-			for(int i=0; i<4; i++) {
-				int nr = curR + dr[i];
-				int nc = curC + dc[i];
-				
-				if(nr<0 || nc<0 || nr>=N || nc>=N) continue;
-			
-				if(dist[nr][nc] > curNode.weight + map[nr][nc]) {
-					dist[nr][nc] = curNode.weight + map[nr][nc];
-					pq.add(new Node(nr, nc, dist[nr][nc]));
-				}
-			}
-			
-		}
-		
-		return dist[N-1][N-1]; ///////////
-	}
+        @Override
+        public int compareTo(Info o) {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+
+    static int[][] map;
+    static int[][] dist;
+    static boolean[][] visited;
+    static final int INF = Integer.MAX_VALUE;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int t = 0;
+        while(true) {
+            t++;
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            if(N == 0) break;
+
+            map = new int[N][N];
+            dist = new int[N][N];
+            visited = new boolean[N][N];
+            for(int i=0; i< N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j=0; j< N; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    dist[i][j] = INF;
+                }
+            }
+
+            sb.append("Problem ").append(t).append(": ").append(solve()).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    private static int solve() {
+        PriorityQueue<Info> pq = new PriorityQueue<>();
+        pq.add(new Info(0, 0, map[0][0]));
+        visited[0][0] = true;
+        dist[0][0] = map[0][0];
+
+        while(!pq.isEmpty()) {
+            Info cur = pq.poll();
+
+            visited[cur.x][cur.y] = true;
+
+            for(int i=0; i<4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(nx < 0 || N <= nx || ny < 0 || N <= ny || visited[nx][ny]) continue;
+
+                if(dist[nx][ny] > cur.w + map[nx][ny]) {
+                    dist[nx][ny] = cur.w + map[nx][ny];
+                    pq.add(new Info(nx, ny, cur.w + map[nx][ny]));
+                }
+
+
+            }
+
+
+        }
+
+        return dist[N-1][N-1];
+    }
 
 }
